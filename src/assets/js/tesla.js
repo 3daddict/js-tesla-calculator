9f1c63159705684eecd47891d5a81e16d28e1931
Michael Salvati <print@3daddict.com>
2019-06-11 21:45:33 -0700

    getBasePrice function

---
+++ src/assets/js/tesla.js
@@ -0,0 +1,26 @@
+const Tesla = {
+    model: {
+        standardRange: 39900,
+        awdLongRange: 49900,
+        awdPerformace: 59900
+    },
+    color: {
+        black: 0,
+        silver: 1000,
+        blue: 1000,
+        white: 1500,
+        red: 1500
+    },
+    wheel: {
+        aero: 0,
+        sport: 1500
+    },
+    interior: {
+        black: 0,
+        white: 1000
+    },
+    upgrade: {
+        streetAutoDrive: 6000
+    }
+
+}
