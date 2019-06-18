143b394cf53dcc4f1b09d054debd5a4152455b80
Michael Salvati <print@3daddict.com>
2019-06-17 21:45:58 -0700

    working to data on dom

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,36 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('JS Loaded!');
-    getBasePrice();
-
-
-
-    
-    
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("JS Loaded!");
+  getBasePrice();
 });
 
-const getBasePrice = () => {
-    document.querySelectorAll(".model-selection-btn").forEach((el) => {
-        el.addEventListener("click", function() {
-          console.log(this.textContent);
-            const standardRange = '39,900';
-            const awdLongRange = '49,900';
-            const awdPerformace = '59,900';
-            const priceText = document.getElementById('modelPrice');
-            switch (this.textContent) {
-                case 'Standard':
-                        priceText.textContent = standardRange;
-                        return standardRange;
-                case 'AWD Long Range':
-                        priceText.textContent = awdLongRange;
-                        return awdLongRange;
-                case 'AWD Performance':
-                        priceText.textContent = awdPerformace;
-                        return awdPerformace;
-                default:
-                    break;
-            }
-        });
-      });
-}
-    
+const Tesla = {
+  model: { standardRange: 39900, awdLongRange: 49900, awdPerformace: 59900 },
+  color: { black: 0, silver: 1000, blue: 1000, white: 1500, red: 1500 },
+  wheel: { aero: 0, sport: 1500 },
+  interior: { black: 0, white: 1000, red: 1500 },
+  upgrade: { streetAutoDrive: 6000 }
+};
 
+const getBasePrice = () => {
+  document.querySelectorAll(".model-selection-btn").forEach(el => {
+    el.addEventListener("click", function() {
+      console.log(this.textContent);
+      const priceText = document.getElementById("modelPrice");
+      switch (this.textContent) {
+        case "Standard":
+          priceText.textContent = Tesla.model.standardRange;
+          return addTotal(Tesla.model.standardRange);
+        case "AWD Long Range":
+          priceText.textContent = Tesla.model.awdLongRange;
+          return addTotal(Tesla.model.awdLongRange);
+        case "AWD Performance":
+          priceText.textContent = Tesla.model.awdPerformace;
+          return addTotal(Tesla.model.awdPerformace);
+        default:
+          break;
+      }
+    });
+  });
+};
+
+const addTotal = modelTotal => {
+  const totalText = document.getElementById("totalPrice");
+  totalText.textContent = modelTotal;
+};
